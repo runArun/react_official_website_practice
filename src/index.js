b8949ce456d87b9699d05300aadb795ac1d99b98
runArun <zl2613185@qq.com>
2017-11-12 10:26:18 +1100

    preState

--- src/index.js
+++ src/index.js
@@ -1,8 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+
+
+import React, { Component }from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Title extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            title : '小红书',
+            count: 0
+
+        }
+    }
+
+    changeTitle(){
+        this.setState({
+            title: '小黄书',
+
+        })
+    }
+    handleClickOnLikeButton () {
+        this.setState((prevState) => {
+            return { count: 0 }
+        })
+        this.setState((prevState) => {
+            return { count: prevState.count + 1 } // 上一个 setState 的返回是 count 为 0，当前返回 1
+        })
+        this.setState((prevState) => {
+            return { count: prevState.count + 2 } // 上一个 setState 的返回是 count 为 1，当前返回 3
+        })
+        // 最后的结果是 this.state.count 为 3
+    }
+    render () {
+        return (
+            <h1 onClick={()=>this.handleClickOnLikeButton ()}> { this.state.count } </h1>
+        )
+    }
+}
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                <Title />
+                <h2>This is Header</h2>
+            </div>
+        )
+    }
+}
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is main content</h2>
+            </div>
+        )
+    }
+}
+
+class Footer extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is footer</h2>
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
 registerServiceWorker();
