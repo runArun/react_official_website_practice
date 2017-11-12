3c790d2520108bfc0786fb54602eace8115671f8
runArun <zl2613185@qq.com>
2017-11-12 11:23:56 +1100

    map

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ class Footer extends Component {
         )
     }
 }
-
+/*
 class Index extends Component {
     render () {
         return (
@@ -80,6 +80,40 @@ class Index extends Component {
                 <Header />
                 <Main />
                 <Footer />
+            </div>
+        )
+    }
+}*/
+
+
+
+const users = [
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Lucy', age: 20, gender: 'female' }
+]
+
+class User extends Component {
+    render () {
+        let { user }  = this.props    // object deconstruction assignment , so we should add {} and use 'user' not others
+        console.log(user)
+        return (
+            <div>
+                <div>姓名：{user.username}</div>
+                <div>年龄：{user.age}</div>
+                <div>性别：{user.gender}</div>
+                <hr />
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                {users.map((user,i) => <User key={i} user={user} />)}
             </div>
         )
     }
